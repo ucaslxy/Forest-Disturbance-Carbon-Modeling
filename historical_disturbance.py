9bbf978216d513aaa8d8b5b4936c3793c5fbc8d6
ucaslixy <26956031+ucaslxy@users.noreply.github.com>
2023-04-17 14:33:01 -0500

    Update and rename Historical_Disturbance.py to historical_disturbance.py

--- historical_disturbance.py
+++ historical_disturbance.py
@@ -1,2 +1,6 @@
 # based on forest age to make an prescribed forest disturbance map during 1860 or 1900 to 1985
 
+import arcpy
+from arcpy import sa
+from arcpy.sa import *
+arcpy.CheckOutExtension("Spatial")
