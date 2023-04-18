3b941753757a7f225218fc331d064b074dd365fc
ucaslixy <26956031+ucaslxy@users.noreply.github.com>
2023-04-18 01:29:26 -0500

    Update forest_disturbance_mapping.js

--- forest_disturbance_mapping.js
+++ forest_disturbance_mapping.js
@@ -1,4 +1,4 @@
-// download nfad fire disturbance data
+
 var MTBS_severity = ee.ImageCollection("USFS/GTAC/MTBS/annual_burn_severity_mosaics/v1");
 var nafd_first = ee.Image("projects/sat-io/open-datasets/NAFD/VCT_30m_first");
 var nafd_last = ee.Image("projects/sat-io/open-datasets/NAFD/VCT_30m_last");
@@ -9,6 +9,20 @@ var dataset = ee.ImageCollection('MODIS/006/MCD12Q1');
 var modlc = dataset.select('LC_Type1').filter(ee.Filter.date('2010-01-01', '2010-12-31')).first();
 var modlc_prj = modlc.reproject(nafd_prj, null, 500);
 var modlc_projection = modlc_prj.projection();
+
+var lcpri_col = ee.ImageCollection("projects/sat-io/open-datasets/LCMAP/LCPRI");
+
+var lcpri_1985 = lcpri_col.filterDate("1985-01-01","1985-12-31").first();
+var lcpri_2010 = lcpri_col.filterDate("2010-01-01","2010-12-31").first();
+
+var lc_forest_1985 = lcpri_1985.eq(4);
+var lc_nforest_20001 = lcpri_1985.lt(4);
+var lc_nforest_20002 = lcpri_1985.gt(4);
+var lc_forest_loss1 = lc_forest_1985.multiply(lc_nforest_20001);
+var lc_forest_loss2 = lc_forest_1985.multiply(lc_nforest_20002);
+var lc_forest_loss = lc_forest_loss1.add(lc_forest_loss2);
+var lc_forest_stable = lc_forest_loss.eq(0);
+var lc_forest_stable_m = lc_forest_stable.reproject(nafd_prj, null, 30);
 
 
 var ini_yr = 1970;
@@ -26,13 +40,17 @@ for(var yid=15; yid <= 40; yid++){
         ": (b('Severity') == 3) ? 1" +
           ": (b('Severity') == 4) ? 1" +
           ": 0");
-  
+  var mtbs_data_m = mtbs_data.reproject(nafd_prj, null, 30);
   // nafd yr
+  // step 1
   var nafd_yr = nafd_first.eq(yid);
-  var nafd_mtbs = nafd_yr.add(mtbs_data);
+  // step 2
+  // var nafd_yr = nafd_last.eq(yid);
+  
+  var nafd_mtbs = nafd_yr.add(mtbs_data_m);
   var nafd_fire = nafd_mtbs.eq(2);
   
-  // Out
+  // Out fire
   var nafd_fire_500 = nafd_fire
     .reduceResolution({
       reducer: ee.Reducer.mean(),
@@ -45,6 +63,28 @@ for(var yid=15; yid <= 40; yid++){
   Export.image.toDrive({
     image: nafd_fire_500,
     description: 'nafd_fire_first_' + yr,
+    scale: 500,
+    maxPixels: 1e13,
+    crs: 'EPSG:4326',
+    folder: '0NAFD',
+    region: seus
+  });
+  
+  
+  // Out fire
+  var nafd_500 = nafd_yr
+    .reduceResolution({
+      reducer: ee.Reducer.mean(),
+      maxPixels: 65536
+    })
+    .reproject({
+      crs: modlc_projection
+    });
+    
+  Export.image.toDrive({
+    image: nafd_500,
+    // description: 'nafd_first_' + yr,
+    description: 'nafd_first_' + yr,
     scale: 500,
     maxPixels: 1e13,
     crs: 'EPSG:4326',
